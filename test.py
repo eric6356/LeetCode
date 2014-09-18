60e7b319b4f0a4981839f62778f16a5776225127
ERIC6356 <eric6356@gmail.com>
2014-09-18 17:50:55 +0800

    没什么进展

--- test.py
+++ test.py
@@ -1,8 +1,23 @@
-ks = dict()
-ks['a'] = {1, 2}
-if 'b' not in ks:
-    ks['b'] = set()
-ks['b'].add(1)
-ks['a'].add(2)
-print ks['a'], ks['b']
-print len(ks['a'])
+import time
+
+# t_start = time.time()
+# a = list()
+# for i in range(100000):
+#     a.append(i)
+# for i in range(100000):
+#     del a[0]
+# t_1 = time.time()
+# for i in range(100000):
+#     a.append(i)
+# for i in range(100000):
+#     a.pop()
+# t_2 = time.time()
+# print t_1 - t_start, t_2 - t_1
+
+t1 = time.time()
+a = range(20000)
+for i in range(10000, 20000):
+    if i in a:
+        pass
+t2 = time.time()
+print t2 - t1
