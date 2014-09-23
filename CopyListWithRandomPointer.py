69492309467464c642aa422b38904b378399d58f
ERIC6356 <eric6356@gmail.com>
2014-09-23 10:55:15 +0800

    CopyListWithRandomPointer 用了Discuss里的算法，先插入，分配Random，再拆出来。另外Python没有do while，导致代码有点累赘。

--- CopyListWithRandomPointer.py
+++ CopyListWithRandomPointer.py
@@ -9,28 +9,34 @@ class RandomListNode:
 #     # @param head, a RandomListNode
 #     # @return a RandomListNode
 def copyRandomList(head):
-        i = 0
-        d = dict()
         if not head:
             return head
-        while head:
-            d[i] = head
-            head = head.next
-            i += 1
-        head = RandomListNode(d[0].label)
-        d_cpy = dict()
-        d_cpy[0] = head
-        n = len(d)
-        for i in range(1, n):
-            d_cpy[i] = RandomListNode(d[i].label)
-            d_cpy[i-1].next = d_cpy[i]
-        for i in range(n):
-            j = 0
-            tmp = d[i].random
-            if not tmp:
-                break
-            while tmp:
-                tmp = tmp.next
-                j += 1
-            d_cpy[i].random = d_cpy[n-j]
+        p = head
+        while p.next:
+            tmp = RandomListNode(p.label)
+            tmp.next = p.next
+            p.next = tmp
+            p = tmp.next
+        tmp = RandomListNode(p.label)
+        tmp.next = p.next
+        p.next = tmp
+        p = head
+        while p.next.next:
+            if p.random:
+                p.next.random = p.random.next
+            else:
+                p.next.random = None
+            p = p.next.next
+        if p.random:
+           p.next.random = p.random.next
+        else:
+            p.next.random = None
+        p = head
+        head = p.next
+        while p.next.next:
+            tmp = p.next
+            p.next = p.next.next
+            p = tmp.next
+            tmp.next = p.next
+        p.next = None
         return head
