765bbe88d0683add94936044ac8445fd7f6a38f9
Eric6356 <eric6356@gmail.com>
2014-09-23 21:26:00 +0800

    SingleNumber2 Discuss黑科技。。。

--- SingleNumber2.py
+++ SingleNumber2.py
@@ -1,33 +1,13 @@
    # @param A, a list of integer
 # @return an integer
 def singleNumber(A):
-    dic_p = dict()
-    dic_n = dict()
-    for num in A:
-        if num > 0:
-            i_b_p = bin(num)[2:]
-            for j in range(len(i_b_p)):
-                if j in dic_p:
-                    dic_p[j] += int(i_b_p[len(i_b_p)-j-1])
-                else:
-                    dic_p[j] = int(i_b_p[len(i_b_p)-j-1])
-        else:
-            i_b_n = bin(num)[3:]
-            for j in range(len(i_b_n)):
-                if j in dic_n:
-                    dic_n[j] += int(i_b_n[len(i_b_n)-j-1])
-                else:
-                    dic_n[j] = int(i_b_n[len(i_b_n)-j-1])
-    result_p = 0
-    result_n = 0
-    for (key, val) in dic_p.items():
-            result_p <<= 1
-            result_p += (val % 3)
-    for (key, val) in dic_n.items():
-            result_n <<= 1
-            result_n += (val % 3)
-    return result_n | result_p
+    ones = 0
+    twos = 0
+    for i in A:
+        ones ^= i & ~twos
+        twos ^= i & ~ones
+    return ones
 
 
-a = [0, 0, 0, 5]
+a = [1, 1, 1, 2, 2, 2, 3]
 print singleNumber(a)
