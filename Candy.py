c944331bb72d4678d1fdd74f9ea1725772bbd3a0
ERIC6356 <eric6356@gmail.com>
2014-09-24 09:23:38 +0800

    Candy TLE!!!

--- Candy.py
+++ Candy.py
@@ -2,55 +2,76 @@
 # # @param ratings, a list of integer
 #     # @return an integer
 def candy(ratings):
-    if not ratings:
-        return 0
-    n = len(ratings)
-    if n == 1:
-        return 1
-    if n == 2:
-        if ratings[0] == ratings[1]:
-            return 2
-        return 3
-    candys = [0] * len(ratings)
-    # done means how many children have been given candy
-    done = 0
-    last_index = list()
-    mmm = [0] * len(ratings)
-    if ratings[0] < ratings[1]:
-        candys[0] = 1
-        done += 1
-        mmm[0] = -1
-        last_index.append(0)
-    if ratings[-1] < ratings[-2]:
-        candys[-1] = 1
-        done += 1
-        mmm[-1] = -1
-        last_index.append(n - 1)
-    for i in range(1, n - 1):
-        if ratings[i - 1] > ratings[i] < ratings[i + 1]:
-            candys[i] = 1
-            done += 1
-            mmm[i] = -1
-            last_index.append(i)
-        elif ratings[i - 1] < ratings[i] > ratings[i + 1]:
-            mmm[i] = 1
-
-    while done < n:
-        this_indx = list()
-        for index in last_index:
+        if not ratings:
+            return 0
+        n = len(ratings)
+        if n == 1:
+            return 1
+        if n == 2:
+            if ratings[0] == ratings[1]:
+                return 2
+            return 3
+        candys = [0] * n
+        min_index = list()
+        mmm = [0] * len(ratings)
+        if ratings[0] < ratings[1]:
+            candys[0] = 1
+            mmm[0] = -1
+            min_index.append(0)
+        if ratings[-1] < ratings[-2]:
+            candys[-1] = 1
+            mmm[-1] = -1
+            min_index.append(n - 1)
+        for i in range(1, n - 1):
+            if ratings[i - 1] >= ratings[i] <= ratings[i + 1]:
+                candys[i] = 1
+                mmm[i] = -1
+                min_index.append(i)
+            elif ratings[i - 1] <= ratings[i] >= ratings[i + 1]:
+                mmm[i] = 1
+        for index in min_index:
             # before index
-            if index > 0 and candys[index - 1] == 0:
-                if ratings[index - 1] > ratings[index]:
-                    candys[index - 1] = candys[index] + 1
-                    this_indx.append()
+            left_index = index - 1
+            while left_index > 0:
+                if not candys[left_index]:
+                    candys[left_index] = candys[left_index+1] + 1
+                    if mmm[left_index] == 1:
+                        break
+                    else:
+                        left_index -= 1
+                        continue
+                if mmm[left_index] == -1:
+                    break
+                print 'left wrong'
+                break
+            # after index
+            right_index = index + 1
+            while right_index < n-1:
+                if not candys[right_index]:
+                    candys[right_index] = candys[right_index-1] + 1
+                    if mmm[right_index] == 1:
+                        break
+                    else:
+                        right_index += 1
+                        continue
+                if mmm[right_index] == -1:
+                    break
                 else:
-                    candys[index - 1] = candys[index] + 1
-
-
-        last_index = this_indx
-        print done
-    print candys
+                    if candys[right_index] <= candys[right_index-1]:
+                        candys[right_index] = candys[right_index-1] + 1
+                    break
+        if not candys[0]:
+            if ratings[0] > ratings[1]:
+                candys[0] = candys[1] + 1
+            else:
+                candys[0] = 1
+        if not candys[-1]:
+            if ratings[-1] > ratings[-2]:
+                candys[-1] = candys[2] + 1
+            else:
+                candys[-1] = 1
+        return sum(candys)
 
 
 r = [1, 2, 2]
-candy(r)
+print candy(r)
