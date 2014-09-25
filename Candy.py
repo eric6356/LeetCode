22749c87b4124de299835f1af94e85155ef196b0
ERIC6356 <eric6356@gmail.com>
2014-09-25 17:36:42 +0800

    Candy 优化了一点，AC!

--- Candy.py
+++ Candy.py
@@ -1,6 +1,6 @@
 # class Solution:
 # # @param ratings, a list of integer
-#     # @return an integer
+# # @return an integer
 def candy(ratings):
         if not ratings:
             return 0
@@ -11,24 +11,29 @@ def candy(ratings):
             if ratings[0] == ratings[1]:
                 return 2
             return 3
-        candys = [0] * n
+        candys = dict()
+        # cdy = [0] * n
         result = 0
         min_index = list()
         mmm = [0] * len(ratings)
-        if ratings[0] < ratings[1]:
-            candys[0] = 1
+        r = 0
+        l = 0
+        if ratings[0] <= ratings[1]:
             result += 1
+            # cdy[0] = 1
             mmm[0] = -1
             min_index.append(0)
-        if ratings[-1] < ratings[-2]:
-            candys[-1] = 1
+            l = 1
+        if ratings[-1] <= ratings[-2]:
             result += 1
+            # cdy[-1] = 1
             mmm[-1] = -1
             min_index.append(n - 1)
+            r = 1
         for i in range(1, n - 1):
             if ratings[i - 1] >= ratings[i] <= ratings[i + 1]:
-                candys[i] = 1
                 result += 1
+                # cdy[i] = 1
                 mmm[i] = -1
                 min_index.append(i)
             elif ratings[i - 1] <= ratings[i] >= ratings[i + 1]:
@@ -36,54 +41,53 @@ def candy(ratings):
         for index in min_index:
             # before index
             left_index = index - 1
+            c = 1
             while left_index > 0:
-                if not candys[left_index]:
-                    candys[left_index] = candys[left_index+1] + 1
-                    result += candys[left_index]
+                if mmm[left_index] >= 0:
+                    c += 1
+                    result += c
+                    # cdy[left_index] = c
                     if mmm[left_index] == 1:
+                        candys[left_index] = c
                         break
                     else:
                         left_index -= 1
                         continue
-                if mmm[left_index] == -1:
-                    break
-                print 'left wrong'
                 break
+            if not l and not left_index:
+                result += c + 1
+                # cdy[0] = c + 1
+        for index in min_index:
             # after index
             right_index = index + 1
-            while right_index < n-1:
-                if not candys[right_index]:
-                    candys[right_index] = candys[right_index-1] + 1
-                    result += candys[right_index]
-                    if mmm[right_index] == 1:
-                        break
+            c = 1
+            while right_index < n - 1:
+                if mmm[right_index] == 0:
+                    c += 1
+                    result += c
+                    # cdy[right_index] = c
+                    right_index += 1
+                    continue
+                elif mmm[right_index] == 1:
+                    if right_index in candys:
+                        # print ratings[right_index], candys[right_index], c
+                        if c >= candys[right_index]:
+                            result += c - candys[right_index] + 1
+                            # cdy[right_index] += c - candys[right_index] + 1
                     else:
-                        right_index += 1
-                        continue
-                if mmm[right_index] == -1:
+                        c += 1
+                        result += c
+                        # cdy[right_index] = c
                     break
-                else:
-                    if candys[right_index] <= candys[right_index-1]:
-                        result += candys[right_index-1] - candys[right_index] + 1
-                        candys[right_index] = candys[right_index-1] + 1
-
-                    break
-        if not candys[0]:
-            if ratings[0] > ratings[1]:
-                candys[0] = candys[1] + 1
-                result += candys[1] + 1
-            else:
-                candys[0] = 1
-                result += 1
-        if not candys[-1]:
-            if ratings[-1] > ratings[-2]:
-                candys[-1] = candys[-2] + 1
-                result += candys[-2] + 1
-            else:
-                candys[-1] = 1
-                result += 1
+                break
+            if not r and right_index == n - 1:
+                result += c + 1
+                # cdy[-1] = c+1
         return result
 
 
-r = [1, 2, 2]
+r = [58, 21, 72, 77, 48, 9, 38, 71, 68, 77, 82, 47, 25, 94, 89, 54, 26, 54, 54, 99, 64, 71, 76, 63, 81, 82, 60, 64, 29,
+     51, 87, 87, 72, 12, 16, 20, 21, 54, 43, 41, 83, 77, 41, 61, 72, 82, 15, 50, 36, 69, 49, 53, 92, 77, 16, 73, 12, 28,
+     37, 41, 79, 25, 80, 3, 37, 48, 23, 10, 55, 19, 51, 38, 96, 92, 99, 68, 75, 14, 18, 63, 35, 19, 68, 28, 49, 36, 53,
+     61, 64, 91, 2, 43, 68, 34, 46, 57, 82, 22, 67, 89]
 print candy(r)
