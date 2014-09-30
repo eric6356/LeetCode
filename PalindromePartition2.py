911d9c766a2f828a68a561dde7e9c93fc60c534e
ERIC6356 <eric6356@gmail.com>
2014-09-30 15:30:16 +0800

    PalindormePartition2 最短路径问题。

---
+++ PalindromePartition2.py
@@ -0,0 +1,45 @@
+   # return weather s is Palindrome
+def isPalindrome( s):
+    n = len(s)
+    return s[:n/2] == s[:(n-1)/2:-1]
+def ooxx(res, index, stk, result):
+    for i in res[index]:
+        if i:
+            stk.append(i)
+            ooxx(res, i-1, stk, result)
+            stk.pop()
+        else:
+            tmp = [0]
+            for j in stk[::-1]:
+                tmp.append(j)
+            result.append(tmp)
+    return result
+# @param s, a string
+# @return a list of lists of string
+def partition( s):
+    n = len(s)
+    if not n:
+        return list()
+    if n == 1:
+        return [s]
+    res = list()
+    for i in range(n):
+        res.append({i+1})
+    for i in range(n):
+        for j in range(i+1, n):
+            if isPalindrome(s[i:j+1]):
+                res[i].add(j+1)
+    stk = list()
+    result = list()
+    ok = list()
+    print res
+    # for cut in ooxx(res, len(res)-1, stk, result):
+    #     tmp = list()
+    #     i_cut = 0
+    #     for i_cut in range(1, len(cut)):
+    #         tmp.append(s[cut[i_cut-1]:cut[i_cut]])
+    #     tmp.append(s[cut[i_cut]:])
+    #     ok.append(tmp)
+    return ok
+s = 'abababababababababa'
+print len(partition(s))
