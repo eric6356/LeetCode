f15a92a4f21daf723c2eace370eb6efe31aac29b
ERIC6356 <eric6356@gmail.com>
2014-10-01 20:31:53 +0800

    PalindromePartition2 O(n^2)找所有的回文序列，然后求最短路径。Still TLE...

--- PalindromePartition2.py
+++ PalindromePartition2.py
@@ -1,45 +1,40 @@
-   # return weather s is Palindrome
-def isPalindrome( s):
+# return weather s is Palindrome
+def isPalindrome(s):
     n = len(s)
-    return s[:n/2] == s[:(n-1)/2:-1]
-def ooxx(res, index, stk, result):
-    for i in res[index]:
-        if i:
-            stk.append(i)
-            ooxx(res, i-1, stk, result)
-            stk.pop()
-        else:
-            tmp = [0]
-            for j in stk[::-1]:
-                tmp.append(j)
-            result.append(tmp)
-    return result
+    return s[:n / 2] == s[:(n - 1) / 2:-1]
+
+
 # @param s, a string
 # @return a list of lists of string
-def partition( s):
+def minCut(s):
     n = len(s)
     if not n:
         return list()
-    if n == 1:
-        return [s]
-    res = list()
+    if isPalindrome(s):
+        return 0
+    connect = list()
     for i in range(n):
-        res.append({i+1})
+        connect.append({i + 1})
     for i in range(n):
-        for j in range(i+1, n):
-            if isPalindrome(s[i:j+1]):
-                res[i].add(j+1)
-    stk = list()
-    result = list()
-    ok = list()
-    print res
-    # for cut in ooxx(res, len(res)-1, stk, result):
-    #     tmp = list()
-    #     i_cut = 0
-    #     for i_cut in range(1, len(cut)):
-    #         tmp.append(s[cut[i_cut-1]:cut[i_cut]])
-    #     tmp.append(s[cut[i_cut]:])
-    #     ok.append(tmp)
-    return ok
-s = 'abababababababababa'
-print len(partition(s))
+        for j in range(i + 1, n)[::-1]:
+            if s[i] == s[j] and j + 1 not in connect[i] and isPalindrome(s[i:j + 1]):
+                for k in range((j - i + 1) / 2):
+                    connect[i + k].add(j + 1 - k)
+    # for i in connect:
+    #     print i
+    cuti = 0
+    now_list = connect[0]
+    next_list = list()
+    while True:
+        cuti += 1
+        for i in now_list:
+            next_list = list()
+            for j in connect[i]:
+                if j == n:
+                    return cuti
+                next_list.append(j)
+        now_list = next_list
+
+
+s = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+print minCut(s)
