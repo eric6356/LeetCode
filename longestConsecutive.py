fd42c33d2ee334d8ea155c9119c9decffad7fedc
ERIC6356 <eric6356@gmail.com>
2014-10-09 15:40:23 +0800

    longestConsecutive 用了sort，AC了，但不是O(n)

--- longestConsecutive.py
+++ longestConsecutive.py
@@ -2,4 +2,15 @@ class Solution:
     # @param num, a list of integer
     # @return an integer
     def longestConsecutive(self, num):
-        res = range(min(num), max(num))
+        num.sort()
+        m = 0
+        tmp = 0
+        for i in range(len(num)-1):
+            if num[i+1] == num[i] + 1:
+                tmp += 1
+            elif num[i+1] == num[i]:
+                pass
+            else:
+                m = max(m, tmp)
+                tmp = 0
+        return max(m, tmp) + 1
--- test.py
+++ test.py
@@ -1,28 +1,16 @@
 def longestConsecutive(num):
-        res = dict()
-        for i in num:
-            if i in res:
-                continue
-            if i-1 in res:
-                if i+1 in res:
-                    res[i-1] += res[i+1]+1
-                    res[i] = res[i-1]
-                    res[i+1] = res[i-1]
-                    continue
-                else:
-                    res[i-1] += 1
-                    res[i] = res[i-1]
-                    continue
-            elif i+1 in res:
-                res[i] = res[i+1] + 1
-                res[i+1] = res[i]
-                continue
-            else:
-                res[i] = 1
+        num.sort()
         m = 0
-        for i in res:
-            m = max(m, res[i])
-        return m
+        tmp = 0
+        for i in range(len(num)-1):
+            if num[i+1] == num[i] + 1:
+                tmp += 1
+            elif num[i+1] == num[i]:
+                pass
+            else:
+                m = max(m, tmp)
+                tmp = 0
+        return max(m, tmp) + 1
 
 num = [100, 4, 200, 1, 3, 2]
 num2 = [1, 2, 0, 1]
@@ -30,3 +18,4 @@ num3 = [1, 3, 5, 2, 4]
 print longestConsecutive(num)
 print longestConsecutive(num2)
 print longestConsecutive(num3)
+print longestConsecutive([0,3,7,2,5,8,4,6,0,1])
