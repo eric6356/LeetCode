909220ff1615c6cb465471813aaff76bd7f84510
ERIC6356 <eric6356@gmail.com>
2014-10-09 11:29:06 +0800

    longestConsecutive TLE!

--- test.py
+++ test.py
@@ -1,45 +1,32 @@
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+def longestConsecutive(num):
+        res = dict()
+        for i in num:
+            if i in res:
+                continue
+            if i-1 in res:
+                if i+1 in res:
+                    res[i-1] += res[i+1]+1
+                    res[i] = res[i-1]
+                    res[i+1] = res[i-1]
+                    continue
+                else:
+                    res[i-1] += 1
+                    res[i] = res[i-1]
+                    continue
+            elif i+1 in res:
+                res[i] = res[i+1] + 1
+                res[i+1] = res[i]
+                continue
+            else:
+                res[i] = 1
+        m = 0
+        for i in res:
+            m = max(m, res[i])
+        return m
 
-
-def proc(node, stk, res):
-    stk.append(node.val)
-    tmp = 0
-    if node.left:
-        proc(node.left, stk, res)
-    if node.right:
-        proc(node.right, stk, res)
-    if not node.left and not node.right:
-        for i in stk:
-            tmp *= 10
-            tmp += i
-        # print stk, node.val, res, tmp
-        stk.pop()
-        res[0] += tmp
-        return res[0]
-    stk.pop()
-    return res[0]
-# @param root, a tree node
-# @return an integer
-def sumNumbers(root):
-    if not root:
-        return 0
-    stk = list()
-    res = [0]
-    return proc(root, stk, res)
-
-
-root = TreeNode(1)
-l2 = TreeNode(2)
-l3 = TreeNode(3)
-l4 = TreeNode(4)
-
-root.left = l2
-root.right = l3
-l2.right = l4
-
-r = TreeNode(0)
-print sumNumbers(r)
+num = [100, 4, 200, 1, 3, 2]
+num2 = [1, 2, 0, 1]
+num3 = [1, 3, 5, 2, 4]
+print longestConsecutive(num)
+print longestConsecutive(num2)
+print longestConsecutive(num3)
