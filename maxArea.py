587d9a8ff3f93ad83e12ef69039052c569a26eeb
ERIC6356 <eric6356@gmail.com>
2014-11-12 15:47:20 +0800

    maxArea Discuss中的On算法

--- maxArea.py
+++ maxArea.py
@@ -1,3 +1,13 @@
 class Solution:
     # @return an integer
     def maxArea(self, height):
+        low = 0
+        high = len(height)-1
+        maxArea = 0
+        while high > low:
+            maxArea = max(maxArea, min(height[low], height[high]) * (high - low))
+            if height[low] > height[high]:
+                high -= 1
+            else:
+                low += 1
+        return maxArea
